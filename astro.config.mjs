615b13a2f70efe66aadfa2c3fc856bcc3a20d4e2
Noel George C J <noelgeorge2004@gmail.com>
2024-12-25 11:15:47 +0530

    Build test

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/Landing-Page/',
   vite: {
     assetsInclude: ["**/*.riv"],
   },
